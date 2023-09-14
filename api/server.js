12402757df475ba24b92b030071a56c8dcd1f6f2
tiagodev07 <tiagobrr17@gmail.com>
2023-09-14 15:02:58 -0400

    first commit

--- api/server.js
+++ api/server.js
@@ -5,11 +5,32 @@ const app = express(),
       bodyParser = require("body-parser");
       port = 3000;
 
+// place holder for the data
+const users = [];
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../my-app/dist')));
 
 app.get('/', (req, res) => {
   res.status(200).json(['work']);
+
+});
+
+app.get('/api/users', (req, res) => {
+  console.log('api/users called!!!!!!!')
+  res.json(users);
+});
+
+app.post('/api/user', (req, res) => {
+  const user = req.body.user;
+  user.id = randomId(10);
+  console.log('Adding user:::::', user);
+  users.push(user);
+  res.json("user addedd");
+});
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
 });
 
 app.listen(port, () => {
